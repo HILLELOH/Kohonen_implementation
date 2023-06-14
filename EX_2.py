5bf34f48385e4467a123b766b2f3a35af92460d9
Hillel Ohayon <95539247+HILLELOH@users.noreply.github.com>
2023-06-14 17:28:45 +0300

    part a - finished

--- EX_2.py
+++ EX_2.py
@@ -44,10 +44,15 @@ def cube(distribution='uniform'):
         input_data = np.random.normal(loc=0.5, scale=0.2, size=(3000, 2))
         input_data = np.clip(input_data, 0, 1)
 
-    elif distribution == 'geometric':
-        p = 0.5
-        input_data = np.random.geometric(p, size=(3000, 2))
-        input_data = input_data / np.max(input_data)  # Scale input_data to the range (0, 1)
+    elif distribution == '(x, y**2)':
+        x = np.random.uniform(size=3000)
+        y = np.random.uniform(size=3000)
+        input_data = np.column_stack((x, y**2))
+
+    elif distribution == '(x**2, y)':
+        x = np.random.uniform(size=3000)
+        y = np.random.uniform(size=3000)
+        input_data = np.column_stack((x**2, y)) 
 
     for epoch in epochs:
         kohonen_few_neurons = Kohonen(map_shape=(4, 5), learning_rate=0.1, sigma=1.0)
@@ -83,27 +88,97 @@ def daunat(distribution='uniform'):
         r1 = np.sqrt(np.random.uniform(size=num_points) * 12 + 4)
         theta = np.random.uniform(low=0, high=2 * np.pi, size=num_points)
 
-    elif distribution == 'normal':
-        pass
+        for epoch in epochs:
+            x = r1 * np.cos(theta)
+            y = r1 * np.sin(theta)
 
-    elif distribution == 'geometric':
-        pass
+            input_data = np.column_stack((x, y))
+            kohonen_many_neurons = Kohonen(map_shape=(50, 6), learning_rate=0.1, sigma=1.0)
+            kohonen_many_neurons.train(input_data, num_epochs=epoch)
+            weights_many_neurons = kohonen_many_neurons.get_weights()
 
-    for epoch in epochs:
-        x = r1 * np.cos(theta)
-        y = r1 * np.sin(theta)
+            plt.scatter(input_data[:, 0], input_data[:, 1], color='blue', label='Input Data')
+            plt.scatter(weights_many_neurons[:, :, 0], weights_many_neurons[:, :, 1], color='red', label='Learned Weights')
+            plt.title(f'Kohonen Network - 300 Neurons - epochs: {epoch}')
+            plt.legend(loc='upper left')
+            plt.show()
 
-        input_data = np.column_stack((x, y))
-        kohonen_many_neurons = Kohonen(map_shape=(50, 6), learning_rate=0.1, sigma=1.0)
-        kohonen_many_neurons.train(input_data, num_epochs=epoch)
-        weights_many_neurons = kohonen_many_neurons.get_weights()
+    elif distribution == 'based x-coordinate':
+        epochs = [1, 500]
+        num_points = 3000
 
-        plt.scatter(input_data[:, 0], input_data[:, 1], color='blue', label='Input Data')
-        plt.scatter(weights_many_neurons[:, :, 0], weights_many_neurons[:, :, 1], color='red', label='Learned Weights')
-        plt.title(f'Kohonen Network - 300 Neurons - epochs: {epoch}')
-        plt.legend(loc='upper left')
-        plt.show()
+        r1 = np.sqrt(np.random.uniform(size=num_points) * 12 + 4)
+        theta = np.random.uniform(low=0, high=2 * np.pi, size=num_points)
+        for epoch in epochs:
+            x = r1 * np.cos(theta)
+            y = r1 * np.sin(theta)
+
+            # Adjust the likelihood of picking a point based on the size of x-coordinate
+            x_weights = np.abs(x) / np.max(np.abs(x))
+            y_weights = np.ones_like(y)  # Uniform likelihood for y-coordinate
+
+            weighted_points = np.column_stack((x_weights, y_weights))
+
+            # Generate data based on the normalized weights
+            indices = np.random.choice(range(num_points), size=num_points, p=weighted_points[:, 0] / np.sum(weighted_points[:, 0]))
+            x_selected = x[indices]
+            y_selected = y[indices]
+
+            input_data = np.column_stack((x_selected, y_selected))
+            kohonen_many_neurons = Kohonen(map_shape=(50, 6), learning_rate=0.1, sigma=1.0)
+            kohonen_many_neurons.train(input_data, num_epochs=epoch)
+            weights_many_neurons = kohonen_many_neurons.get_weights()
+
+            plt.scatter(input_data[:, 0], input_data[:, 1], color='blue', label='Input Data')
+            plt.scatter(weights_many_neurons[:, :, 0], weights_many_neurons[:, :, 1], color='red', label='Learned Weights')
+            plt.title(f'Kohonen Network - 300 Neurons - epochs: {epoch}')
+            plt.legend(loc='upper left')
+            plt.show()
+
+
+    elif distribution == 'based y-coordinate':
+        epochs = [1, 1000]
+        num_points = 3000
+
+        r1 = np.sqrt(np.random.uniform(size=num_points) * 12 + 4)
+        theta = np.random.uniform(low=0, high=2 * np.pi, size=num_points)
+        for epoch in epochs:
+            x = r1 * np.cos(theta)
+            y = r1 * np.sin(theta)
+
+            # Adjust the likelihood of picking a point based on the size of y-coordinate
+            x_weights = np.ones_like(x)  # Uniform likelihood for x-coordinate
+            y_weights = np.abs(y) / np.max(np.abs(y))
+
+            weighted_points = np.column_stack((x_weights, y_weights))
+
+            # Generate data based on the normalized weights
+            indices = np.random.choice(range(num_points), size=num_points, p=weighted_points[:, 1] / np.sum(weighted_points[:, 1]))
+            x_selected = x[indices]
+            y_selected = y[indices]
+
+            input_data = np.column_stack((x_selected, y_selected))
+            kohonen_many_neurons = Kohonen(map_shape=(50, 6), learning_rate=0.1, sigma=1.0)
+            kohonen_many_neurons.train(input_data, num_epochs=epoch)
+            weights_many_neurons = kohonen_many_neurons.get_weights()
+
+            plt.scatter(input_data[:, 0], input_data[:, 1], color='blue', label='Input Data')
+            plt.scatter(weights_many_neurons[:, :, 0], weights_many_neurons[:, :, 1], color='red', label='Learned Weights')
+            plt.title(f'Kohonen Network - 300 Neurons - epochs: {epoch}')
+            plt.legend(loc='upper left')
+            plt.show()
+
+
+
 
 if __name__ == '__main__':
     cube(distribution='uniform')
+    cube(distribution='normal')
+    cube(distribution='(x, y**2)')
+    cube(distribution='(x**2, y)')
+
+    daunat(distribution='uniform')
+    daunat(distribution='based x-coordinate')
+    daunat(distribution='based y-coordinate')
+    
 
